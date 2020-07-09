2b0210b02aaf7022a8543ec8543ce4fba8702cc6
OlekSt <alexander.stepaniuk@yahoo.com.ua>
2020-07-09 15:24:57 +0300

    Some minor changes

--- src/test/kotlin/com/example/springjpapostgresqlsecurity/SpringJpaPostgreSQLSecurityApplicationTests.kt
+++ src/test/kotlin/com/example/springjpapostgresqlsecurity/SpringJpaPostgreSQLSecurityApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringJpaPostgresqlSecurityApplicationTests {
+class SpringJpaPostgreSQLSecurityApplicationTests {
 
 	@Test
 	fun contextLoads() {
